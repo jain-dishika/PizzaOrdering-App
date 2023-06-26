3aec2687102941db0c47c2577bee10710264510d
Dishika <dishikajain5@gmail.com>
2023-06-26 14:42:16 +0530

    Home Page Completed

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,28 +1,98 @@
 import 'package:my_app/consts/consts.dart';
-
+import 'package:my_app/consts/lists.dart';
+import 'package:my_app/widgets_common/home_buttons.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(12),
       color: lightGrey,
       width: context.screenWidth,
       height: context.screenHeight,
       child: SafeArea(child: Column(
         children: [
           Container(
+            height: 60,
+            alignment: Alignment.center,
             color: lightGrey,
             child: TextFormField(
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.search),
                 filled: true,
                 fillColor: whiteColor,
-                hintText: 
+                hintText: searchAnything,
+                hintStyle: TextStyle(color: textfieldGrey),
               ),
             ),
-          )
+          ),
+          10.heightBox,
+          Expanded(child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column( children: [
+            VxSwiper.builder(
+            aspectRatio: 16/9,
+            autoPlay: true,
+            height: 150,
+            enlargeCenterPage: true,
+            itemCount: slidersList.length, 
+            itemBuilder: (context, index){
+            return Image.asset(
+              slidersList[index],
+              fit: BoxFit.fill,
+              ).box.rounded.clip(Clip.antiAlias).margin(const EdgeInsets.symmetric(horizontal: 8)).make();
+          }),
+          10.heightBox,
+          Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children : List.generate(2, (index) => homeButtons(
+              height: context.screenHeight * 0.1,
+              width: context.screenWidth/2.5,
+              icon: index == 0 ? icTodaysDeal : icFlashDeal,
+              title: index == 0 ? todayDeal : flashsale,
+            ))
+          ),
+          10.heightBox,
+          VxSwiper.builder(
+            aspectRatio: 16/9,
+            autoPlay: true,
+            height: 150,
+            enlargeCenterPage: true,
+            itemCount: secondSlidersList.length, 
+            itemBuilder: (context, index){
+            return Image.asset(
+              secondSlidersList[index],
+              fit: BoxFit.fill,
+              ).box.rounded.clip(Clip.antiAlias).margin(const EdgeInsets.symmetric(horizontal: 8)).make();
+          }),
+          10.heightBox,
+          Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children : List.generate(3, (index) => homeButtons(
+              height: context.screenHeight * 0.15,
+              width: context.screenWidth/3.5,
+              icon: index == 0 
+                ? icTopCategories 
+                : index == 1 
+                  ? icBrands : icTopSeller,
+              title: index == 0
+              ? topCategories 
+              : index == 1 ?
+              brand : icBrands,
+            )),
+          ),
+          10.heightBox,
+          Align(
+              alignment: Alignment.centerLeft,
+              child : featuredCategories.text.color(darkFontGrey).size(18).fontFamily(semibold).make(),
+            ),
+          ],)
+          ))
         ],
-      )),
+      )
+      ),
+
     );
   }
 }
